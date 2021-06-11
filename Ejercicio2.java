962095d7d064d02922daa24e955758b76dc41b0b
¨Jose <josechavarrial811@gmail.com>
2021-06-10 21:08:14 -0600

    ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class Ejercicio2 {
+
+	public static void main(String args[])  {
+		Scanner scanner = new Scanner(System.in);
+		int n;
+		System.out.println("N?");
+		n = scanner.nextInt();
+		while (n>0) {
+			System.out.println(n);
+			n = n-1;
+		}
+	}
+
+
+}
+
